6476f556f30bc6d649b64e92fd39786aae90c9c9
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2024-01-28 19:26:42 +0800

    FEAT: Add client object proof of concept

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import tomllib
 
 from rcon import Console
+from rcon.async_support import Console as AsyncConsole
 
 import logger
 
@@ -35,19 +36,148 @@ def send_command_fallback(command: str):
 
 
 # ------------------------------------------------------------------------------
+# Synchronous implementation; manually starts and stops a connection with every command
 class Client:
     def __init__(self):
+        self.GENERIC_ERROR = "Unable to process your request (server did not respond)"
+        log.info("Setting up RCON connection")
+        self.CONFIG = fetch_config()
+
+    def open(self):
+        return Console(
+            host=self.CONFIG["ip"],
+            password=self.CONFIG["password"],
+            port=self.CONFIG["port"],
+            timeout=self.CONFIG["timeout_duration"]
+        )
+
+    # Admin Commands:
+    def info(self):
+        log.debug("Fetching server info")
+        console = self.open()
+        res = console.command("Info")
+        console.close()
+        return res if res else self.GENERIC_ERROR
+
+    def save(self):
+        log.debug("Saving world")
+        console = self.open()
+        res = console.command("Save")
+        console.close()
+        return res if res else self.GENERIC_ERROR
+
+    def online(self):
+        # Response is of format `name,playerid,steamid`
+        log.debug("Fetching online players")
+        console = self.open()
+        res = console.command("ShowPlayers")
+        console.close()
+        # TODO: REFORMAT INTO MORE READABLE OUTPUT
+        return res if res else self.GENERIC_ERROR
+
+    def announce(self, message: str):
+        log.debug("Broadcasting message to world")
+        console = self.open()
+        res = console.command(f"Broadcast {message}")
+        console.close()
+        # TODO: Consider reformatting server's response
+        return res if res else self.GENERIC_ERROR
+
+    def kick(self, steam_id: str):
+        log.debug("Kicking player from server")
+        console = self.open()
+        res = console.command(f"KickPlayer {steam_id}")
+        console.close()
+        return res if res else self.GENERIC_ERROR
+
+    def ban(self, steam_id: str):
+        log.debug("Banning player from server")
+        console = self.open()
+        res = console.command(f"BanPlayer {steam_id}")
+        console.close()
+        return res if res else self.GENERIC_ERROR
+
+    def shutdown(self, seconds: str, message: str):
+        log.debug(f"Schedule server shutdown in {seconds} seconds")
+        console = self.open()
+        res = console.command(f"Shutdown {seconds} {message}")
+        console.close()
+        return res if res else self.GENERIC_ERROR
+
+    def force_stop(self):
+        log.debug(f"Terminating the server forcefully")
+        console = self.open()
+        res = console.command(f"DoExit")
+        console.close()
+        # TODO: Check if this is supposed to give a response (and alter accordingly)
+        return res if res else self.GENERIC_ERROR
+
+
+# ------------------------------------------------------------------------------
+# Async implementation; connection remains open throughout lifetime
+# This is listed as experimental on the library's docs, for use with Discord bots
+# In my testing, it doesn't receive the correct number of bytes as of 28th Jan 2024
+class AsyncClient:
+    def __init__(self):
+        self.GENERIC_ERROR = "Unable to process your request (server did not respond)"
         log.info("Setting up RCON connection")
         config = fetch_config()
-        self.CONSOLE = Console(
+        self.CONSOLE = AsyncConsole(
             host=config["ip"],
             password=config["password"],
             port=config["port"],
             timeout=config["timeout_duration"]
         )
 
-    def close(self):
+    async def check_console_ready(self):
+        if not self.CONSOLE.is_open():
+            await self.CONSOLE.open()
+
+    async def close(self):
         log.info("Closing RCON connection")
-        self.CONSOLE.close()
+        await self.CONSOLE.close()
 
     # Admin Commands:
+    async def info(self):
+        log.debug("Fetching server info")
+        await self.check_console_ready()
+        res = await self.CONSOLE.command("Info")
+        return res if res else self.GENERIC_ERROR
+
+    async def save(self):
+        await self.check_console_ready()
+        res = await self.CONSOLE.command("Save")
+        return res if res else self.GENERIC_ERROR
+
+    async def online(self):
+        # Response is of format `name,playerid,steamid`
+        await self.check_console_ready()
+        res = await self.CONSOLE.command("ShowPlayers")
+        # TODO: REFORMAT INTO MORE READABLE OUTPUT
+        return res if res else self.GENERIC_ERROR
+
+    async def announce(self, message: str):
+        await self.check_console_ready()
+        res = await self.CONSOLE.command(f"Broadcast {message}")
+        # TODO: Consider reformatting reply
+        return res if res else self.GENERIC_ERROR
+
+    async def kick(self, steam_id: str):
+        await self.check_console_ready()
+        res = await self.CONSOLE.command(f"KickPlayer {steam_id}")
+        return res if res else self.GENERIC_ERROR
+
+    async def ban(self, steam_id: str):
+        await self.check_console_ready()
+        res = await self.CONSOLE.command(f"BanPlayer {steam_id}")
+        return res if res else self.GENERIC_ERROR
+
+    async def shutdown(self, seconds: str, message: str):
+        await self.check_console_ready()
+        res = await self.CONSOLE.command(f"Shutdown {seconds} {message}")
+        return res if res else self.GENERIC_ERROR
+
+    async def force_stop(self):
+        await self.check_console_ready()
+        res = await self.CONSOLE.command(f"DoExit")
+        return res if res else self.GENERIC_ERROR
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
+import asyncio
 import sys
 
 import client
 import logger
 
 log = logger.get_logger(__name__)
-#
-#
-# Initial implementation
-# def test_get_info():
-#     try:
-#         with client.get_client() as con:
-#             response = con.run("Info")
-#             log.debug(response)
-#     except timeout as e:
-#         log.error("Unable to fetch server info!\n", e)
 
 
-def fallback_get_info():
-    client.send_command_fallback("Info")
+async def test_async_connection():  # for testing purposes only
+    server = client.AsyncClient()  # init using config files
+    log.debug(await server.info())
+    log.debug(await server.online())
+
+
+async def test_connection():  # proof of concept
+    server = client.Client()
+    log.debug(server.info())
+    log.debug(server.online())
 
 
 if __name__ == "__main__":
@@ -26,7 +25,8 @@ if __name__ == "__main__":
         logger.shutdown_logger()
         sys.exit(0)
 
-    fallback_get_info()
+    # REMOVE THIS AFTER TESTING:
+    asyncio.run(test_connection())
     
     logger.shutdown_logger()
     sys.exit(0)
