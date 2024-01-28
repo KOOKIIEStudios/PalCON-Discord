013f58472e7bcfeaaacf9edf454f892977597fed
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2024-01-28 18:07:06 +0800

    FEAT: Add info example

--- client.py
+++ client.py
@@ -1,5 +1,39 @@
-from rcon.source import Client
+import tomllib
+
+# from rcon.source import Client  # initial, using: https://pypi.org/project/rcon/
+from rcon import Console
+
+import logger
+
+log = logger.get_logger(__name__)
 
 
-def get_client(ip="127.0.0.1", port=25575, password="", timeout_duration=3):
-    return Client(ip, port, passwd=password, timeout=timeout_duration)
+# fetch config
+def fetch_config():
+    with open("config.toml", "rb") as file:
+        data = tomllib.load(file)
+    if data:
+        return data
+    log.error("Unable to read configuration file!")
+
+
+CONFIG = fetch_config()
+
+
+# ------------------------------------------------------------------------------
+# Fallback
+def send_command_fallback(command: str):
+    con = Console(
+        host=CONFIG["ip"],
+        password=CONFIG["password"],
+        port=CONFIG["port"],
+        timeout=CONFIG["timeout_duration"]
+    )
+    res = con.command(command)
+    con.close()
+
+    log.debug(res)
+    return res
+# Initial RCON library wrapper:
+# def get_client(ip="127.0.0.1", port=25575, password="", timeout_duration=3):
+#     return Client(ip, port, passwd=password, timeout=timeout_duration)
--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import sys
-from socket import timeout
 
 import client
 import logger
 
 log = logger.get_logger(__name__)
+#
+#
+# Initial implementation
+# def test_get_info():
+#     try:
+#         with client.get_client() as con:
+#             response = con.run("Info")
+#             log.debug(response)
+#     except timeout as e:
+#         log.error("Unable to fetch server info!\n", e)
 
 
-def get_info():
-    try:
-        with client.get_client() as con:
-            response = con.run("Info")
-            log.debug(response)
-    except timeout as e:
-        log.error("Unable to fetch server info!\n", e)
+def fallback_get_info():
+    client.send_command_fallback("Info")
 
 
 if __name__ == "__main__":
-    get_info()
+    test_fetch_config = client.fetch_config()
+    if not test_fetch_config:
+        logger.shutdown_logger()
+        sys.exit(0)
+
+    fallback_get_info()
     
     logger.shutdown_logger()
     sys.exit(0)
